dc6fbdd034a4b280f9869dc4de0d1d2889089698
StrawberryBeds <samuel.l.wood@gmail.com>
2025-04-05 23:09:51 -0400

    Alignement horizontale des elements d'Header et le bon path de l'icon VdM

--- src/composents/Header.jsx
+++ src/composents/Header.jsx
@@ -1,12 +1,16 @@
-import favicon32 from "/Users/samuelwood/Documents/WebApps/WebAppsProjet1/src/assets/favicon-32x32.png"
+import favicon32 from './../assets/favicon-32x32.png';
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CiSearch } from "react-icons/ci";
 import { RiAccountCircleLine } from "react-icons/ri";
 
+// To Do 1: Spacing and styling
+// To Do 2: The header is static when the page is scrolled.
+
+
 function Header() {
 
     return (
-        <>
+      <div className="header-container">
         <h1>Montréal</h1>
         <a href="https://react.dev" target="_blank">
           <img src={favicon32} className="logo react" alt="Montréal logo" />
@@ -17,7 +21,7 @@ function Header() {
         <p>Recherche</p>
         <RiAccountCircleLine />
         <p>Mon Compte</p> 
-        </> 
+        </div> 
     )
 }
 
