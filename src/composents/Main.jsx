8660f1287f75322d0f44e8ab1fae32204f5d6a1c
StrawberryBeds <samuel.l.wood@gmail.com>
2025-04-06 01:51:27 -0400

    Ajoute barre de recherche au main

--- src/composents/Main.jsx
+++ src/composents/Main.jsx
@@ -1,13 +1,42 @@
-function Main () {
+import { useState } from 'react'
+
+function Main() {
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
 
     return (
         <>
-        <h1>Avis et Alertes</h1>
-        <h2>Trouver un avis</h2>
-        <div class="input-group-icon input-group-icon-left">
-            <input type="text" class="form-control input-rounded get-query" id="search-input" data-query="q" placeholder="Que cherchez-vous?" aria-label="Recherche" value=""></input>
-            <span class="icon icon-search" aria-hidden="true"></span>
+            <div class="container position-relative">
+                <div class="row document-heading-header">
+                    <div class="col-12 col-lg-7 align-self-center">
+                        <div class="pb-2">
+                            <h1 class="header-full-width-title">Avis et alertes</h1>
+                            <div class="document-heading-subtitle rm-last-child-mb">Trouver un avis</div>
+                        </div>
+                        <div class="form-group form-group-lg">
+                            <div class="input-group-icon input-group-icon-left">
+                                <input type="text"
+                                    class="form-control input-rounded get-query"
+                                    id="search-input"
+                                    data-query="q"
+                                    placeholder="Que cherchez-vous?"
+                                    aria-label="Recherche"
+                                    value={searchQuery}
+                                    onChange={handleInputChange}>
+                                </input>
+
+                                <span class="icon icon-search" aria-hidden="true"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
         </>
     )
 }
