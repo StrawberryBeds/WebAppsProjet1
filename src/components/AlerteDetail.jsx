33d3493b2a897372e93b263d3aeda598c97b6b25
StrawberryBeds <samuel.l.wood@gmail.com>
2025-04-27 15:45:41 -0400

    Ajoute d'un aside au AlerteDetail.

--- src/components/AlerteDetail.jsx
+++ src/components/AlerteDetail.jsx
@@ -1,14 +1,22 @@
+import React from 'react';
+import './AlerteDetail.css';
+
 function AlerteDetail({ alert }) {
     return (
-        <div>
+        <div className="aside-grid-container">
+            <div className="main-content">
             <h2>{alert.properties.titre}</h2>
-            <p>Start Date: {new Date(alert.properties.date_debut).toLocaleString()}</p>
-            <p>End Date: {new Date(alert.properties.date_fin).toLocaleString()}</p>
-            <p>Type: {alert.properties.type}</p>
-            <p>Publisher: {alert.properties.service_publieur}</p>
-            {/* <a href={alert.properties.lien} target="_blank" rel="noopener noreferrer">
+                <p>Start Date: {new Date(alert.properties.date_debut).toLocaleString()}</p>
+                <p>End Date: {new Date(alert.properties.date_fin).toLocaleString()}</p>
+                <p>Type: {alert.properties.type}</p>
+                <p>Publisher: {alert.properties.service_publieur}</p>
+                {/* <a href={alert.properties.lien} target="_blank" rel="noopener noreferrer">
                 {alert.properties.lien}
             </a> */}
+            </div>
+            <aside className="aside">
+            <h3>S'abonner aux alertes</h3>
+            </aside>
         </div>
     );
 }
