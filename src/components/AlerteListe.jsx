7f9b318310c61fce3d0e7c3ed66516a88fad9780
StrawberryBeds <samuel.l.wood@gmail.com>
2025-04-27 15:04:35 -0400

    Fait le div alerte entier un NavLink

--- src/components/AlerteListe.jsx
+++ src/components/AlerteListe.jsx
@@ -3,48 +3,50 @@ import './AlerteListe.css';
 import { NavLink } from 'react-router-dom';
 
 function AlerteListe({ searchQuery, data }) {
-    const [filteredItems, setFilteredItems] = useState(data?.features || []);
+  const [filteredItems, setFilteredItems] = useState(data?.features || []);
 
-    useEffect(() => {
-        if (searchQuery) {
-          const filtered = data.features.filter((feature) =>
-            feature.properties.titre.toLowerCase().includes(searchQuery.toLowerCase())
-          );
-          setFilteredItems(filtered);
-        } else {
-          setFilteredItems(data.features);
-        }
-      }, [searchQuery, data]);
-      
+  useEffect(() => {
+    if (searchQuery) {
+      const filtered = data.features.filter((feature) =>
+        feature.properties.titre.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredItems(filtered);
+    } else {
+      setFilteredItems(data.features);
+    }
+  }, [searchQuery, data]);
 
-    return (
-      <div className="alertes-container">
-              {/* This can be put inside the NavLink to conform to the reference site. */}
-                {filteredItems.length > 0 ? ( 
-                filteredItems.map((feature) => (
-                  <div className="alerte">
-                    <div key={feature.id}>
-                        <h3>{feature.properties.titre}</h3>
-                        {/* <p>Start Date: {new Date(feature.properties.date_debut).toLocaleString()}</p> */}
-                        {/* <p>End Date: {new Date(feature.properties.date_fin).toLocaleString()}</p> */}
-                        {/* <p>Type: {feature.properties.type}</p> */}
-                        {/* <p>Publisher: {feature.properties.service_publieur}</p> */}
-                        <NavLink
-                        to={`/alerte/${feature.id}`}
-                        className="nav-link" 
-                        >Afficher l'alerte
-                        </NavLink>
 
-                        {/* <a href={feature.properties.lien} target="_blank" rel="noopener noreferrer"> {feature.properties.lien}
+  return (
+    <div className="alertes-container">
+      {/* This can be put inside the NavLink to conform to the reference site. */}
+      {filteredItems.length > 0 ? (
+        filteredItems.map((feature) => (
+          <div className="alerte">
+            <NavLink
+              to={`/alerte/${feature.id}`}
+              className="nav-link"
+            >
+              <div key={feature.id}>
+                <h3>{feature.properties.titre}</h3>
+                {/* <p>Start Date: {new Date(feature.properties.date_debut).toLocaleString()}</p> */}
+                {/* <p>End Date: {new Date(feature.properties.date_fin).toLocaleString()}</p> */}
+                {/* <p>Type: {feature.properties.type}</p> */}
+                {/* <p>Publisher: {feature.properties.service_publieur}</p> */}
+                Afficher l'alerte
+              </div>
+            </NavLink>
+
+            {/* <a href={feature.properties.lien} target="_blank" rel="noopener noreferrer"> {feature.properties.lien}
                         </a> */}
-                        </div>
-                    </div>
-                ))
-              ) : (
-                <p>Rien à afficher</p>
-              )}
-            </div>
-    );
+
+          </div>
+        ))
+      ) : (
+        <p>Rien à afficher</p>
+      )}
+    </div>
+  );
 }
 
 export default AlerteListe;
