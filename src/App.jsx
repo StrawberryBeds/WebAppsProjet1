ff3cf05f8bf0eb46954abfafdb51464a45f2c8d0
StrawberryBeds <samuel.l.wood@gmail.com>
2025-06-01 19:28:29 -0400

    Suppression de App.css en preference de index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
