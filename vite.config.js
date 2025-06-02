521f0103f014d2873299fe128844affda61b60fe
StrawberryBeds <samuel.l.wood@gmail.com>
2025-06-01 23:11:28 -0400

    Updates vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -10,6 +9,8 @@ export default defineConfig({
       strategies: 'injectManifest',
       srcDir: 'src',
       filename: 'service-worker.js',
+      swSrc: 'src/sw.js', // Source file
+      swDest: 'dist/service-worker.js', // Destination file
       injectManifest: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
@@ -30,4 +31,3 @@ export default defineConfig({
     }),
   ],
 });
-
