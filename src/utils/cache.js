179acb874bafe7e940fc223e2fe5e8405a80dfaf
StrawberryBeds <samuel.l.wood@gmail.com>
2025-06-01 21:54:47 -0400

    Changes to cache.js

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,15 +1,33 @@
 export function getFromCache(key) {
-    return JSON.parse(localStorage.getItem(key)) ?? [];
+    try {
+        const data = localStorage.getItem(key);
+        return data ? JSON.parse(data) : null;
+    } catch (error) {
+        console.error(`Error retrieving data from cache for key ${key}:`, error);
+        return null;
+    }
 }
 
 export function saveInCache(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
+    try {
+        localStorage.setItem(key, JSON.stringify(data));
+    } catch (error) {
+        console.error(`Error saving data to cache for key ${key}:`, error);
+    }
 }
 
 export function removeFromCache(key) {
-    localStorage.removeItem(key);
+    try {
+        localStorage.removeItem(key);
+    } catch (error) {
+        console.error(`Error removing data from cache for key ${key}:`, error);
+    }
 }
 
 export function clearCache() {
-    localStorage.clear();
+    try {
+        localStorage.clear();
+    } catch (error) {
+        console.error('Error clearing cache:', error);
+    }
 }
